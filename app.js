6aef207aa3b6ac14cdbcbf98a8ca401fce468204
DhavitG <gdhavit@gmail.com>
2025-03-27 19:28:45 +0530

    Controllers and routes setup

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 require("dotenv").config();
-
+require("express-async-errors");
 const express = require("express");
 const app = express();
+
+// connect DB
+
+// importing routers
+const authRouter = require("./routes/auth");
+const jobRouter = require("./routes/jobs");
+
+// importing middlewares
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+
+app.use(express.json());
+
+// routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5002;
 
