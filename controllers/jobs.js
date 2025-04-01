2bd0b1e6bdb16f5a62e6197f27ebaf63ba6c960f
DhavitG <gdhavit@gmail.com>
2025-04-01 18:05:37 +0530

    Get and Post routes done

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,17 +1,30 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors/index");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-  res.send("get a job");
+  const { userId } = req.user;
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) throw new NotFoundError(`No job with id ${jobId}`);
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  console.log("Received token, req.user:", req.user);
-  if (!req.user) {
-    return res.status(401).json({ error: "Authentication failed" });
-  }
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
